ea6b0baf4cac704400ab6175c850a2a1112baec5
staylev <62988955+staylev@users.noreply.github.com>
2020-05-27 18:14:11 +0700

    задание2

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+     $Iphone=60000;
+     $galaxy = 56568;
+     $Iphone2=52152;
+     $s=$Iphone+$galaxy+$Iphone2;
+     $nds=$s*0.2;
+     echo ("сумма $s Рублей НДС $nds Рублей");
+     
+ 
+   
+?>
